0199cea727882bd05d4386385d0d3744b7fcb6e7
Aveline Swan <i@xswan.net>
2015-01-07 06:50:20 +0800

    move yum install out of install.bash to make packages cachable

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM centos:latest
 
+RUN yum makecache && \
+  yum groupinstall -y "Development Tools" && \
+  yum install -y gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison iconv-devel tar which
+
 ADD install.bash /install.bash
 
 RUN /install.bash
--- install.bash
+++ install.bash
@@ -1,7 +1,5 @@
 #!/bin/bash -x
 set -e
-yum groupinstall -y "Development Tools"
-yum install -y gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison iconv-devel 
 gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 curl -L get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh

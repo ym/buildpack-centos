46ecdf1126f2709d710666292aafcc9d0b61ec53
Aveline Swan <i@xswan.net>
2015-01-07 05:50:24 +0800

    import mpapis public key

--- install.bash
+++ install.bash
@@ -2,6 +2,7 @@
 set -e
 yum groupinstall -y "Development Tools"
 yum install -y gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison iconv-devel 
+gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 curl -L get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
 rvm install 2.0.0-p598

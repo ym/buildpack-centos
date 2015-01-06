1e1d50b4c785145885108f770e79373b44cf31f7
Aveline Swan <i@xswan.net>
2015-01-07 06:36:33 +0800

    install rvm requirements

--- install.bash
+++ install.bash
@@ -1,5 +1,8 @@
 #!/bin/bash -x
 set -e
+yum groupinstall -y "Development Tools"
+yum install -y gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison iconv-devel
+yum install -y which tar
 gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 curl -L get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
